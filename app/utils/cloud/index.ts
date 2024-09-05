dc92c4a37836d5375bf7c691b8595ab3d3ed4c7d
果农 <htmambo@gmail.com>
2024-09-05 17:12:40 +0800

    同步丢失的文件

--- app/utils/cloud/index.ts
+++ app/utils/cloud/index.ts
@@ -1,17 +1,14 @@
 import { createWebDavClient } from "./webdav";
 import { createUpstashClient } from "./upstash";
-import { createCustomRESTClient } from "./customrest";
 
 export enum ProviderType {
   WebDAV = "webdav",
   UpStash = "upstash",
-  CustomREST = "CustomREST",
 }
 
 export const SyncClients = {
   [ProviderType.UpStash]: createUpstashClient,
   [ProviderType.WebDAV]: createWebDavClient,
-  [ProviderType.CustomREST]: createCustomRESTClient,
 } as const;
 
 type SyncClientConfig = {
