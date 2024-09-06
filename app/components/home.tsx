7aeac75da0533736bc7c4ac178b329615101fe0d
果农 <htmambo@gmail.com>
2024-09-06 09:39:49 +0800

    remove ar lang ...

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -194,7 +194,7 @@ function Screen() {
     <div
       className={`${styles.container} ${
         shouldTightBorder ? styles["tight-container"] : styles.container
-      } ${getLang() === "ar" ? styles["rtl-screen"] : ""}`}
+      } }`}
     >
       {renderContent()}
     </div>
