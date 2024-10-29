fa63664404976ca9f68ca392e9ed27f16d36e7ab
果农 <htmambo@gmail.com>
2024-10-29 11:52:44 +0800

    yyy

--- app/utils/cors.ts
+++ app/utils/cors.ts
@@ -1,5 +1,5 @@
 import { getClientConfig } from "../config/client";
-import { ApiPath, DEFAULT_API_HOST } from "../constant";
+import { ApiPath } from "../constant";
 
 export function corsPath(path: string) {
   const baseUrl = getClientConfig()?.isApp ? `${DEFAULT_API_HOST}` : "";
